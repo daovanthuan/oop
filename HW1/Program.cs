a0eca441ba03bc2a05a2330df33185d4c6270ba9
daovanthuan <144333029+daovanthuan@users.noreply.github.com>
2023-09-10 22:47:45 +0700

    Add files via upload

---
+++ HW1/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine("Họ Và Tên: Đào Văn Thuận");
+Console.WriteLine("Mã Sinh Viên: 12422015");
+Console.WriteLine("Lớp: 12422TN");
+Console.WriteLine("Usename: daovanthuan");
+Console.WriteLine("Địa Chỉ Email: daovanthuan211004@gmail.com");
